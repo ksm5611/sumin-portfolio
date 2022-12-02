aa4c376a175c568b6d55c94051178e43b0b38b47
sumin <kim.sumin.wat2014@gmail.com>
2022-12-01 22:55:49 -0800

    finished python and react setting

---
+++ my-app/src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from "./components/Header";
+// import Projects from "./components/Projects";
+// import Skills from "./components/Skills";
+
+const App = () => {
+  return (
+    <div>
+      <Header />
+      {/* <Skills />
+      <Projects /> */}
+    </div>
+  );
+};
+
+export default App;
