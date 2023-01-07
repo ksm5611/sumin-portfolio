01867bb41a560807166187ec9ffe969c23e941ed
sumin <kim.sumin.wat2014@gmail.com>
2023-01-06 19:56:21 -0800

    clean up dead code

--- my-app/src/components/Projects.js
+++ my-app/src/components/Projects.js
@@ -1,43 +1,7 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import ProjectCard from "./ProjectCard";
-import projects from "../apis/projects";
 
 const Projects = (props) => {
-  // const [projectInfo, setProjectInfo] = useState([]);
-
-  // const fetchProjects = async () => {
-  //   const response = await projects.get("/projects/");
-  //   console.log("here", response);
-  //   setProjectInfo(response.data);
-  // };
-
-  // const createProjectCards = () => {
-  //   return (
-  //     <div className="ui three stackable raised cards">
-  //       {projectInfo.map((project) => {
-  //         if (!project) {
-  //           return <div>Loading..</div>;
-  //         } else {
-  //           return (
-  //             <ProjectCard
-  //               name={project.name}
-  //               description={project.description}
-  //               image={project.image}
-  //               github={project.github}
-  //               demo={project.demo}
-  //               key={project.name}
-  //             />
-  //           );
-  //         }
-  //       })}
-  //     </div>
-  //   );
-  // };
-
-  // useEffect(() => {
-  //   fetchProjects();
-  // }, []);
-
   return (
     <>
       <div className="ui center aligned container">
@@ -50,7 +14,6 @@ const Projects = (props) => {
         github={props.github}
         demo={props.demo}
       />
-      {/* {createProjectCards()} */}
     </>
   );
 };
