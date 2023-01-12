6c97e11b692067a71d52d6a53237c211b21f0a72
sumin <kim.sumin.wat2014@gmail.com>
2023-01-11 17:53:06 -0800

    updated header and intro component into App.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,15 +1,18 @@
 import React from "react";
-import Header from "./components/Header";
+import { BrowserRouter as Router } from "react-router-dom";
+import Header from "./components/Header/Header";
 import Projects from "./components/Projects";
 import Skills from "./components/Skills";
+import Intro from "./components/Intro/Intro";
 
 const App = () => {
   return (
-    <div>
+    <Router>
       <Header />
+      <Intro />
       <Skills />
       <Projects />
-    </div>
+    </Router>
   );
 };
 
