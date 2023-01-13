0ac828a65b51cdca1005547237675c4ef3ed898c
sumin <kim.sumin.wat2014@gmail.com>
2023-01-12 19:59:20 -0800

    fetched skill images data to about me section

---
+++ my-app/src/data/ProjectData.js
@@ -0,0 +1,60 @@
+// --------------------------------------- Skills
+
+export const stackList = [
+  {
+    img: require("../images/html.png"),
+    name: "HTML",
+  },
+  {
+    img: require("../images/css.png"),
+    name: "CSS",
+  },
+  {
+    img: require("../images/pythondjango.png"),
+    name: "Python/Django",
+  },
+  {
+    img: require("../images/bootstrap.jpeg"),
+    name: "Bootstrap",
+  },
+  {
+    img: require("../images/jquery.png"),
+    name: "JQuery",
+  },
+  {
+    img: require("../images/rubyrail.png"),
+    name: "RubyonRail",
+  },
+  {
+    img: require("../images/ajax.png"),
+    name: "AJAX",
+  },
+  {
+    img: require("../images/js.png"),
+    name: "Javascript",
+  },
+  {
+    img: require("../images/sequelize.png"),
+    name: "Sequelize",
+  },
+  {
+    img: require("../images/postgreSql.png"),
+    name: "PostgreSQL",
+  },
+  {
+    img: require("../images/react.png"),
+    name: "React",
+  },
+  {
+    img: require("../images/mysql.png"),
+    name: "MySQL",
+  },
+  {
+    img: require("../images/mongodb.png"),
+    name: "MongoDB",
+  },
+  {
+    img: require("../images/nodejs.png"),
+    name: "Node",
+  },
+];
