f41cfa1a7a02405f8afecaac95ee7e57b4c3d2f0
sumin <kim.sumin.wat2014@gmail.com>
2023-01-17 13:29:36 -0800

    added footer section

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,9 +3,10 @@ import "./App.css";
 import { BrowserRouter as Router } from "react-router-dom";
 import Header from "./components/Header/Header";
 import Intro from "./components/Intro/Intro";
-import Projects from "./components/Projects";
+import Projects from "./components/Projects/Projects";
 import AboutMe from "./components/Aboutme/AboutMe";
 import Contact from "./components/Contact/Contact";
+import Footer from "./components/Footer/Footer";
 
 const App = () => {
   return (
@@ -15,6 +16,7 @@ const App = () => {
       <Projects />
       <AboutMe />
       <Contact />
+      <Footer />
     </Router>
   );
 };
