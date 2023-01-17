c606078e198f70d945dec714cdd6cde3c6449c3d
sumin <kim.sumin.wat2014@gmail.com>
2023-01-16 16:44:24 -0800

    applied contact section and contact element

---
+++ my-app/src/components/Contact/Contact.js
@@ -0,0 +1,29 @@
+import React from "react";
+import SocialIcon from "../SocialIcon/SocialIcon";
+import { ContactWrapper, Email } from "./ContactElements";
+
+function Contact() {
+  return (
+    <ContactWrapper id="contact">
+      <div className="Container">
+        <div className="SectionTitle">Get In Touch</div>
+        <div className="BigCard">
+          <Email>
+            <span>kim.sumin.wat2014@gmail.com</span>
+            <a
+              className="btn PrimaryBtn"
+              href="mailto:kim.sumin.wat2014@gmail.com"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Send Mail
+            </a>
+          </Email>
+        </div>
+        <SocialIcon />
+      </div>
+    </ContactWrapper>
+  );
+}
+
+export default Contact;
