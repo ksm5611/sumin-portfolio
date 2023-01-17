71f80e3c26a3f6089c8e76e1a13bfd59bf51bb85
sumin <kim.sumin.wat2014@gmail.com>
2023-01-16 16:58:17 -0800

    upgrade bootstrap version and import App css to App.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "./App.css";
 import { BrowserRouter as Router } from "react-router-dom";
 import Header from "./components/Header/Header";
 import Intro from "./components/Intro/Intro";
