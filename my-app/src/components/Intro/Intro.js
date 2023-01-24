51e37f45df72a7c84ee582959d216ed31107c3b5
sumin <kim.sumin.wat2014@gmail.com>
2023-01-23 22:29:32 -0800

    changed scroll down to scroll up/style and cleaned code

--- my-app/src/components/Intro/Intro.js
+++ my-app/src/components/Intro/Intro.js
@@ -1,47 +1,54 @@
-import React from "react";
-// import Dropdown from "../Dropdown/Dropdown";
-// import Header from "../Header/Header";
+import React, { useState } from "react";
+import ArrowCircleUpOutlinedIcon from "@mui/icons-material/ArrowCircleUpOutlined";
+import Dropdown from "../Dropdown/Dropdown";
+import Header from "../Header/Header";
 import {
   IntroContainer,
   IntroWrapper,
   IntroLeft,
-  IntroRight,
-  Image,
-  ScrollDown,
   ScrollLink,
+  ScrollUp,
+  Arrow,
 } from "./IntroElements";
 
 const Intro = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => {
+    setIsOpen(!isOpen);
+  };
   return (
-    <main>
-      <IntroContainer>
-        <IntroWrapper>
-          <IntroLeft>
-            <h1>Hi, I'm Sumin Kim</h1>
-            <h5>Full Stack Web Developer</h5>
-            <p>
-              I am an enthusiastic, responsible and hard-working as a software
-              developer.
-            </p>
-          </IntroLeft>
-          {/* <IntroRight>
-            <Image
-              src="https://gurupawar.github.io/portfolio/assets/man-svgrepo-com.svg"
-              alt="man-svgrepo"
-            />
-          </IntroRight> */}
-        </IntroWrapper>
-        <ScrollDown to="projects">
+    <>
+      <main id="nav">
+        <Dropdown isOpen={isOpen} toggle={toggle} />
+        <Header toggle={toggle} />
+        <IntroContainer>
+          <IntroWrapper>
+            <IntroLeft>
+              <h1>Hi, I'm Sumin Kim</h1>
+              <h5>Full Stack Web Developer</h5>
+              <p>
+                I am an enthusiastic, responsible and hard-working as a software
+                developer.
+              </p>
+            </IntroLeft>
+            {/* <IntroRight>
+              <Image
+                src="https://gurupawar.github.io/portfolio/assets/man-svgrepo-com.svg"
+                alt="man-svgrepo"
+              />
+            </IntroRight> */}
+          </IntroWrapper>
+        </IntroContainer>
+      </main>
+      <Arrow>
+        <ScrollUp to="nav">
           <ScrollLink>
-            Scroll down
-            {/* <img
-              src="https://raw.githubusercontent.com/gurupawar/portfolio/main/assets/scroll-down.svg"
-              alt="scroll-down"
-            /> */}
+            <ArrowCircleUpOutlinedIcon fontSize="40px" />
           </ScrollLink>
-        </ScrollDown>
-      </IntroContainer>
-    </main>
+        </ScrollUp>
+      </Arrow>
+    </>
   );
 };
 
