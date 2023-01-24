4b622ed237783790dad8c5d9321c33172922932a
sumin <kim.sumin.wat2014@gmail.com>
2023-01-23 22:54:49 -0800

    cleaned unused code

--- my-app/src/components/Projects/Projects.js
+++ my-app/src/components/Projects/Projects.js
@@ -16,16 +16,6 @@ const Projects = (props) => {
           />
         </div>
       </div>
-      {/* <div className="ui center aligned container" id="projects">
-        <h1 className="SectionTitle">Projects</h1>
-      </div> */}
-      {/* <ProjectCard
-        name={props.name}
-        description={props.description}
-        image={props.image}
-        github={props.github}
-        demo={props.demo}
-      /> */}
     </>
   );
 };
